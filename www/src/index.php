90fe971bb59e704155f16d5b2dec9ae0e1b77de5
moises <correodemomuar@gmail.com>
2021-11-07 22:22:37 +0100

    entorno Docker + prueba composer y bd

--- www/src/index.php
+++ www/src/index.php
@@ -15,6 +15,23 @@
 
         $new = new Zona("cd",4,5,6);
         echo $new;
+
+        $server = "mysql-pr200"; 
+        $user = "root"; 
+        $password = "root"; 
+        $database = "zonas"; 
+        $db = new mysqli($server,$user, $password,$database); 
+        if($db->connect_error){ 
+            die("La conexión con la bd ha fallado, error: " . $db->connect_errno . ": ". $db->connect_error); 
+        } 
+
+        $sentencia = $db->prepare(" SELECT * FROM `Z400` ");     
+        $sentencia -> execute();
+        $sentencia -> bind_result($idregistro, $fh, $cantluz);
+        while( $sentencia->fetch() ){
+            echo "<br>";
+            echo "$idregistro, $fh, $cantluz<br>";
+        }
     ?>
     <header>
 
