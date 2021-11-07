9bfdb0c304f35b56117803d4869a0a222cfde9ac
moises <correodemomuar@gmail.com>
2021-11-07 17:49:39 +0100

    principales directorios + bd

---
+++ src/index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <meta name="description" content="Control Zonas">
+    <link rel="stylesheet" href="../css/style.css">
+</head>
+<body>
+    
+    <header>
+
+    </header>
+
+    <main>
+
+    </main>
+
+    <footer>
+
+    </footer>
+    <script src="./js/script.js" language="javascript" type="text/javascript"></script>
+    
+</body>
+</html>
