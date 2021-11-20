486676db5147c52ea6dd00fffec4b4f086218d41
moises <correodemomuar@gmail.com>
2021-11-20 12:47:52 +0100

    styles-sections-requestJS

--- app/src/index.php
+++ app/src/index.php
@@ -6,44 +6,54 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
     <meta name="description" content="Control Zonas">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@300;400;500&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="../css/style.css">
 </head>
-<body>
+<body class="body">
     <?php
         require '../vendor/autoload.php';
         use Moi\Zonas\Zona;
 
-        $new = new Zona("cd",4,5,6);
-        echo $new;
+        // $new = new Zona("cd",4,5,6);
+        // echo $new;
 
-        $server = "mysql-pr200"; 
-        $user = "root"; 
-        $password = "rpass"; 
-        $database = "zonas"; 
-        $db = new mysqli($server,$user, $password,$database); 
-        if($db->connect_error){ 
-            die("La conexión con la bd ha fallado, error: " . $db->connect_errno . ": ". $db->connect_error); 
-        } 
+        // $server = "mysql-pr200"; 
+        // $user = "root"; 
+        // $password = "rpass"; 
+        // $database = "zonas"; 
+        // $db = new mysqli($server,$user, $password,$database); 
+        // if($db->connect_error){ 
+        //     die("La conexión con la bd ha fallado, error: " . $db->connect_errno . ": ". $db->connect_error); 
+        // } 
 
-        $sentencia = $db->prepare(" SELECT * FROM `Z400` ");     
-        $sentencia -> execute();
-        $sentencia -> bind_result($idregistro, $fh, $cantluz);
-        while( $sentencia->fetch() ){
-            echo "<br>";
-            echo "$idregistro, $fh, $cantluz<br>";
-        }
+        // $sentencia = $db->prepare(" SELECT * FROM `Z400` ");     
+        // $sentencia -> execute();
+        // $sentencia -> bind_result($idregistro, $fh, $cantluz);
+        // while( $sentencia->fetch() ){
+        //     echo "<br>";
+        //     echo "$idregistro, $fh, $cantluz<br>";
+        // }
     ?>
     <header>
 
     </header>
 
-    <main>
+    <main class="main">
+
+        <section class ="section" id="section1">aaaaaaa</section>
+        <section class ="section" id="section2">bbbbbbb</section>
+        <section class ="section" id="section3">ccccc</section>
+        <section class ="section" id="section4">ddddddd</section>
+        <section class ="section" id="section5">eeeeeee</section>
 
     </main>
 
     <footer>
 
     </footer>
+
     <script src="./js/script.js" language="javascript" type="text/javascript"></script>
     
 </body>
---
+++ app/src/js/script.js
@@ -0,0 +1,26 @@
+// obtengo la secciones para imprimir en ellas
+const section1 = document.querySelector('section1');
+const section2 = document.querySelector('section2');
+const section3 = document.querySelector('section3');
+const section4 = document.querySelector('section4');
+const section5 = document.querySelector('section5');
+
+//defino variable para contener json solicitado
+let jsonObjeto;
+
+//url solicitud y tipo
+const requestURL = "path/datosArduino";
+const request = new XMLHttpRequest();
+
+// inicio solicitud, defino respuesta esperada
+// request.open('GET', requestURL );
+// request.responseType = 'json';
+
+// Enviar solicitud al server, parametro para send, json? para solicitud tipo PUT
+// request.send();
+request.onload = function(){
+    // objeto json recibido
+    const jsonObjeto = request.response;
+    // function(obj);
+}
+
